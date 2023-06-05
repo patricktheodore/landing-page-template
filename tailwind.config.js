ec834d35aa910d9029efdc55fa813ce30d36a3d4
patricktheodore <patricktheodoresara@gmail.com>
2023-06-05 16:15:02 +0800

    tailwind init

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{astro,js,jsx,ts,tsx}'],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+

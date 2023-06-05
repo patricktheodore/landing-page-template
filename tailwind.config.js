414ce3e1b5f32a837468f6f450bfd05815b2a97d
patricktheodore <patricktheodoresara@gmail.com>
2023-06-05 17:58:54 +0800

    dummy init

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,26 @@
 module.exports = {
   content: ['./src/**/*.{astro,js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+				'bg-light': '#F5F5F4',
+        'bg-dark': '#0F1D2E',
+        'brand-primary': '#1EC0AD',
+        'brand-secondary': '#028785',
+        'brand-tertiary': '#203B62',
+			},
+    },
   },
   variants: {
-    extend: {},
+    extend: {
+      colors: {
+				'bg-light': '#F5F5F4',
+        'bg-dark': '#0F1D2E',
+        'brand-primary': '#1EC0AD',
+        'brand-secondary': '#028785',
+        'brand-tertiary': '#203B62',
+			},
+    },
   },
   plugins: [],
 }
